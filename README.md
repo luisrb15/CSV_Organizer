2fc598942a093faa228aa7babe7fccbb716dcc21
Luis <luis.becerra.15.12@gmail.com>
2022-07-18 01:03:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## CSV Organizer for S3 Bucket 
+
+This code allows you to take the last CSV file in a S3 bucket, process it and copy into a new S3 bucket.
+Can be used in a Lambda function, triggered by the upload of a new CSV file.
--- main.py
+++ main.py
@@ -1,45 +1,33 @@
-import csv
+import boto3
+import datetime as dt
 
-rows = []
-with open('ATBootcamp03 - PB - 01.csv', 'r') as f:
-    reader = csv.reader(f)
-    header = next(reader)
+year = dt.datetime.now().year
+month = dt.datetime.now().month
+day = dt.datetime.now().day
 
-    ## for every row, replace \n\n with " "
-    for row in reader:
-        rows.append(row)
-        for i in range(len(row)):
-            row[i] = row[i].replace('\n\n', ' ')
-            row[i] = row[i].replace('\n', ' ')
-            row[i] = row[i].replace('\r', ' ')
-            row[i] = row[i].replace('\t', ' ')
-            row[i] = row[i].replace('\xa0', ' ')
-            row[i] = row[i].replace('\u200b', ' ')
-            row[i] = row[i].replace('\u200c', ' ')
-            row[i] = row[i].replace('\u200d', ' ')
-            row[i] = row[i].replace('\u200e', ' ')
-            row[i] = row[i].replace('\u200f', ' ')
-            row[i] = row[i].replace('\u202a', ' ')
-            row[i] = row[i].replace('\u202b', ' ')
-            row[i] = row[i].replace('\u202c', ' ')
-            row[i] = row[i].replace('\u202d', ' ')
-            row[i] = row[i].replace('\u202e', ' ')
-            row[i] = row[i].replace('\u2060', ' ')
-            row[i] = row[i].replace('\u2061', ' ')
-            row[i] = row[i].replace('\u2062', ' ')
-            row[i] = row[i].replace('\u2063', ' ')
-            row[i] = row[i].replace('\u2064', ' ')
-            row[i] = row[i].replace('\u2065', ' ')
-            row[i] = row[i].replace('\u2066', ' ')
-            row[i] = row[i].replace('\u2067', ' ')
-            row
-print(header)
-print(rows)
+original_s3_bucket = ## YOUR S3 BUCKET HERE ##
+destination_s3_bucket = ## YOUR DESTINATION S3 BUCKET HERE ##
 
-## update the csv file
-with open('ATBootcamp03 - PB - 01.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerows(rows)
+s3 = boto3.client("s3")
 
-f.close()
+last_modified = s3.list_objects(Bucket=original_s3_bucket)["Contents"][0]
+for key in s3.list_objects(Bucket=original_s3_bucket)["Contents"]:
+    if key["LastModified"] > last_modified["LastModified"]:
+        last_modified = key
+last_modified_key = last_modified["Key"]
+
+obj = s3.get_object(Bucket=original_s3_bucket, Key= last_modified_key) # get last object in s3 bucket
+file_name = last_modified_key.split("/")[-1] # get last part of key
+    
+
+body = obj["Body"].read().decode("utf-8")
+body = body.replace("\n\n", " ")
+body = body.replace("\n", " ")
+header = body.split("\n")[0]
+rows = body.split("\n")[1:]
+
+destination_key = f"{year}/{month}/{day}/{file_name}" # create new key
+
+destination_body = header + "\n" + "\n".join(rows) # join header and rows
+
+s3.put_object(Bucket=destination_s3_bucket, Key=destination_key, Body=destination_body)
