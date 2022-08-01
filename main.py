1ab253dd3eda11fe8873563d01ae94b3cd12080c
Luis <luis.becerra.15.12@gmail.com>
2022-07-31 23:08:38 -0300

    feat: Add column

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import boto3
 import datetime as dt
 import logging
+
+# from regex import F
 from FileManager import FileManager
 import re
 logger = logging.getLogger()
@@ -14,21 +16,9 @@ original_s3_bucket = 'bootcamps-2022'
 destination_s3_bucket = 'bootcamps-2022-results'
 
 s3 = boto3.client("s3")
-athena_client = boto3.client('athena')
 
 objects_original_list = s3.list_objects(Bucket=original_s3_bucket)["Contents"]
 file_manager = FileManager(original_s3_bucket, s3)
-def LastModifiedFiles():
-    utc_now = dt.datetime.utcnow()
-    utc_minus_30 = utc_now - dt.timedelta(seconds=30)
-    last_uploaded_keys = []
-    for object in objects_original_list:
-        object_creation = object['LastModified']
-        object_creation = object_creation.replace(tzinfo=None)
-        if object_creation > utc_minus_30:
-            last_uploaded_keys.append(object['Key'])
-    logger.info('Last uploaded keys: ' + str(last_uploaded_keys))
-    return last_uploaded_keys
 
 def ObtainFileNames(last_uploaded_keys):
     file_names = []
@@ -66,10 +56,10 @@ def ListBootamps(file_names):
             body = header + "\n" + new_row
             s3.put_object(Bucket=destination_s3_bucket, Key=destination_folder + destination_file_name, Body=body)
 
-def CSVProcessor(last_uploaded_keys, file_names):
+def CSVProcessor(keys, file_names):
     logger.info("Started CSVProcessor")
     number_of_file = 0
-    for key in last_uploaded_keys:
+    for key in keys:
         obj = s3.get_object(Bucket=original_s3_bucket, Key= key) # get last object in s3 bucket
         file_name = file_names[number_of_file]
         number_of_file += 1
@@ -85,17 +75,30 @@ def CSVProcessor(last_uploaded_keys, file_names):
                 break
         header = rows[0]
         rows.remove(header)
-
+        body = AddColumn(file_name, header, rows)
         destination_key = f"{year}/{month}/{day}/{file_name}" # create new key
 
-        destination_body = header + "\n" + "\n".join(rows) # join header and rows
+        destination_body = body
         s3.put_object(Bucket=destination_s3_bucket, Key=f'modified/{file_name}', Body=destination_body)
         s3.put_object(Bucket=destination_s3_bucket, Key=destination_key, Body=destination_body)
 
+def AddColumn(file_name, header, rows):
+    header = f'{header},Bootcamp name'
+    bootcamp_name = file_name.split("_")[0]
+    aux = 0
+    for row in rows:
+        row = row.replace("\n","")
+        newRow = f'{row},{bootcamp_name}'
+        rows.remove(row)
+        rows.insert(aux, newRow)
+        aux += 1
+    body = header + "\n" + "\n".join(rows)
+    return body
+
 def main():
-    last_uploaded_keys = file_manager.get_all_csvs('ATBootcamp')
-    file_names = ObtainFileNames(last_uploaded_keys)
-    CSVProcessor(last_uploaded_keys, file_names)
+    keys = file_manager.get_all_csvs('ATBootcamp')
+    file_names = ObtainFileNames(keys)
+    CSVProcessor(keys, file_names)
     ListBootamps(file_names)
 
 def lambda_handler(event, context):
